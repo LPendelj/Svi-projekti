2d610cf5fb087fa2c8e4c8cf5f011ab597fd6a4e
LPendelj <luka93palace@hotmail.com>
2020-06-13 23:04:45 +0200

    Druga verzija

--- Civot/src/civotIgra/Controller.java
+++ Civot/src/civotIgra/Controller.java
@@ -1,10 +1,16 @@
 package civotIgra;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,13 +18,29 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.stage.Window;
+import model.Datasource;
 
-public class Controller {
+public class Controller implements Initializable{
+
+	//public static List<Cinjenice> cinjenice = new LinkedList<Cinjenice>(); 
+	public static List<Odgovori> listaOdgovora = new LinkedList<Odgovori>();
+	public static LinkedList<Igraci> listaIgraca= new LinkedList<Igraci>();
+	public static List<Igraci> tabela = new LinkedList<Igraci>();
+	
+	private static String imeIgraca;
+	public static String getImeIgraca() {
+		return imeIgraca;
+	}
+
 	@FXML
-	private  ComboBox combo1;
+	private  ComboBox<String> combo1;
+	
+	
+	
+	@FXML
+	private TextField odgovor;
 	
 	@FXML
 	private Button dugmeOk;
@@ -27,59 +49,71 @@ public class Controller {
 	private  TextField polje;
 	
 	@FXML
+	public Label cinjenica;
+	
+	@FXML
+	public  Label oznakaUnos;
+	
+	@FXML
 	public  Label oznaka;
 	
+	@FXML
+	private Button pokreni;
 	
-	private static String imeIgraca;
-	public static String getImeIgraca() {
-		return imeIgraca;
-	}
-
 	@FXML
 	private static  int brojIgraca;
 
+
+	
 	@FXML
 	public static   int getBrojIgraca() {
 		return brojIgraca;
 	}
 	
 	@FXML
-	public  ComboBox getCombo1() {
+	public  ComboBox<String> getCombo1() {
 		return combo1;
 	}
 	
 	@FXML
-	public void setCombo1(ComboBox combo1) {
+	public void setCombo1(ComboBox<String> combo1) {
 		this.combo1 = combo1;
 	}
 	
 	//metod za unosenje broja igraca
 	@FXML
 	public void onClick() {
-		
 		 brojIgraca= Integer.parseInt((String) getCombo1().getValue());
-		 
-			}
+		}
 	
-	
-
 	//pozivamo metod za menjanje scene
 	
 	public void promeniScenu(ActionEvent event) throws IOException {
-		
-		
+				
 		Parent root2 = FXMLLoader.load(getClass().getResource("Window2.fxml"));
 		Scene secondaryStage = new Scene(root2);
-		
-		
 		//uzimamo informacije  Stage-a
 		Stage window = (Stage)(((Node) event.getSource()).getScene().getWindow());
 		window.setScene(secondaryStage);
 		
+		window.show();
+		
+	}
+	
+	
+	public void pokreniIgru(ActionEvent event) throws IOException, ClassNotFoundException {
+		
+		
+		Parent root3 = FXMLLoader.load(getClass().getResource("WindowUnos.fxml"));
+		Scene secondaryStage = new Scene(root3);
+		//uzimamo informacije  Stage-a
+		Stage window = (Stage)(((Node) event.getSource()).getScene().getWindow());
+		window.setScene(secondaryStage);
 		
 		window.show();
 		
-		
+
+
 	}
 	
 	
@@ -91,26 +125,36 @@ public class Controller {
 		this.oznaka=oznaka;
 	}
 
+	
 	//metod za ubacivanje imena
-	public void unesiIme(ActionEvent e) {
+	public void unesiIme(ActionEvent e) throws ClassNotFoundException {
 		imeIgraca = polje.getText();
+				
+		Igraci igrac = new Igraci(imeIgraca);
+		listaIgraca.add(igrac);
 		polje.clear();
-	}
-	
-	//metod u kome sadrzaj oznake dobija redni broj igraca koji je na redu
-
-	public void promeniOznaku() {
-		for (int i=0; i<getBrojIgraca(); i++) {
-			oznaka.setText("Igrac broj " + i + ", upisi svoje ime:");
+		int b = listaIgraca.indexOf(igrac);		
+		if (b+1<brojIgraca)
+		{
+			oznaka.setText("Igrac broj " + (b+2) + ", upisi svoje ime:");
 		}
-
+		else {oznaka.setText("Nastavi dalje.");
+		polje.setDisable(true);
+	//	dugmeOk.setDisable(true);
+		pokreni.setVisible(true);
+		}	
 	}
 	
-//public void initialize(URL url, RescourceBundle rb) {
-//	
-//}
+	public void pokret(MouseEvent e) {
+	String a = Igra.cinjenice.get(0).getSadrzaj();
+	cinjenica.setText(a);
+	}
+	
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
 
-	
-	
-	
 }
+	
+	
+
--- Civot/src/civotIgra/Igra.java
+++ Civot/src/civotIgra/Igra.java
@@ -9,18 +9,16 @@ import java.util.List;
 import java.util.Scanner;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import model.Datasource;
 
 
 public class Igra extends Application{
 	
-	public void start(Stage primaryStage) throws Exception{
+	public void start(Stage primaryStage) throws IOException{
 		Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
 		
 		primaryStage.setTitle("Baronija kviz");
@@ -28,77 +26,59 @@ public class Igra extends Application{
 		primaryStage.show();
 	}
 	
-	/*public void changeScene(Stage primaryStage) throws Exception {
-		Parent root2 = FXMLLoader.load(getClass().getResource("Window2.fxml"));
-		
-		
-		primaryStage.setScene(new Scene(root2, 800,600));
-		primaryStage.show();
-	}*/
+
 	
-	//public static List<Cinjenice> cinjenice = new LinkedList<Cinjenice>();
+	public static List<Cinjenice> cinjenice = new LinkedList<Cinjenice>();
 	public static List<Odgovori> listaOdgovora = new LinkedList<Odgovori>();
 	public static List<Igraci> listaIgraca = new LinkedList<Igraci>();
 	public static List<Igraci> tabela = new LinkedList<Igraci>();
 	
 	public static void main(String[] args) throws ClassNotFoundException {
 		
-		launch(args);
 		Datasource datasource = new Datasource();
 		datasource.open();
 		
-		List<Cinjenice> cinjenice= datasource.preuzmi();
+		cinjenice= datasource.preuzmi();
 		
 		datasource.close();
+		
+		Collections.shuffle(cinjenice);
+		
+		launch(args);
+	
 		
 		
 		Scanner sc = new Scanner(System.in);
 		
 // Ovde je isao stari kod
 		
-		Collections.shuffle(cinjenice);
 		
 		
-// System.out.println("BARONIJA KVIZ\n");
 		
-		System.out.println("Izaberi do 5 igraca!");
+System.out.println("BARONIJA KVIZ\n");
+		
+	 System.out.println("Izaberi do 5 igraca!");
 
-		int brojIgraca= Controller.getBrojIgraca();
-	
-		
-		//secondaryStage.setTitle("Baronija kviz");
-		//secondaryStage.setScene(new Scene(root2, 800,600));
-		//secondaryStage.show();
-		
-		
-		
-		//int brojIgraca=sc.nextInt();
+	int brojIgraca= Controller.getBrojIgraca();
+	/*	int brojIgraca=sc.nextInt();
 		if (brojIgraca>5) {
 			brojIgraca=5;
 		}  else if (brojIgraca<1) {
 			brojIgraca=1;
 		}
-		
-		
-		
+				
 		System.out.println();
 		System.out.println("Izabrano je " + brojIgraca + " igraca.");
-		System.out.println();
+		System.out.println();*/
 		
-		for (int i = 1; i<=brojIgraca; i++) {
-			
-			
-			System.out.println("Igrac broj " + i + ":\nUpisi svoje ime");
-			
-			
-			String imeIgraca = Controller.getImeIgraca();
-			
+	/*	for (int i = 1; i<=brojIgraca; i++) {
+			System.out.println("Igrac broj " + i + ":\nUpisi svoje ime");			
+			String imeIgraca = Controller.getImeIgraca();			
 			System.out.println();
-			
-			Igraci igrac = new Igraci(imeIgraca);
-			
+			Igraci igrac = new Igraci(imeIgraca);			
 			listaIgraca.add(igrac);
-		}
+		}*/
+		
 		System.out.println("Igra pocinje\n");
 		
 		
@@ -111,12 +91,12 @@ public class Igra extends Application{
 		
 			
 		for (int i = 0; i<brojIgraca; i++) {
-			System.out.println("Na redu je " + listaIgraca.get(i).getIme());
+			System.out.println("Na redu je " + Controller.listaIgraca.get(i).getIme());
 			System.out.println("Nakon sto dobijes tvrdnju, upisi odgovor koji mislis da je tacan u prazno polje.");		
 			System.out.println(cinjenice.get(j).getSadrzaj());
 			Odgovori odgovorIgraca = new Odgovori(sc.next());
 			listaOdgovora.add(odgovorIgraca);
-			listaIgraca.get(i).setLazniOdgovor(odgovorIgraca.getSadrzaj());
+			Controller.listaIgraca.get(i).setLazniOdgovor(odgovorIgraca.getSadrzaj());
 		}
 		
 		listaOdgovora.add(cinjenice.get(j).getOdgovor());
@@ -130,12 +110,12 @@ public class Igra extends Application{
 		}
 		
 		for (int i = 0; i<brojIgraca; i++) {
-		System.out.println("\n" + listaIgraca.get(i).getIme() + ", izaberi tacan odgovor.");
+		System.out.println("\n" + Controller.listaIgraca.get(i).getIme() + ", izaberi tacan odgovor.");
 		String pokusajOdgovora = sc.next();
 		if (pokusajOdgovora.equals(cinjenice.get(j).getOdgovor().getSadrzaj())) {
-				listaIgraca.get(i).tacanOdgovor();
+				Controller.listaIgraca.get(i).tacanOdgovor();
 			};
-		for(Igraci ig : listaIgraca) {
+		for(Igraci ig : Controller.listaIgraca) {
 			if (ig.getLazniOdgovor().equals(pokusajOdgovora)){
 				ig.igracPrevaren();
 			}
@@ -160,7 +140,7 @@ public class Igra extends Application{
 		
 		System.out.println("Igra je gotova.\n");
 		System.out.println("Tabela:");
-		for (Igraci ig: listaIgraca) {
+		for (Igraci ig: Controller.listaIgraca) {
 			System.out.println(ig.getIme() + ": " + ig.getBrojPoena());
 		}
 		
@@ -168,56 +148,13 @@ public class Igra extends Application{
 
 		sc.close();
 		
-			
-		/*while(!izlaz) {
-			System.out.println("ukucaj BROJ svog izbora! Ukucaj 0 da izlistas koje su opcije.");
-			izbor = sc.nextInt();
-			sc.nextLine();
-			
-			switch(izbor) {
-				case 0: 
-					ispisiOpcije();
-					break;
-				case 1:
-					spisakZaLidl.prikaziSpisak();
-					break;
-				case 2:
-					System.out.println("izaberi namirnicu koju ces dodati na spisak!");
-					String stvar = skener.nextLine();
-					spisakZaLidl.dodajNaSpisak(stvar);
-					break;
-				case 3:
-					promeniListu();
-					break;
-				case 4:
-					izbaciSaSpiska();
-					break;
-				case 5:
-					nadjiArtikal();
-					break;
-				case 6:
-					izlaz = true;
-					break;
-			}*/
-			
-		
-		
-		
+				
 		
 		
 		}
 		
 		
 	
-	public static void ispisiOpcije() {
-        System.out.println("\nPritisni ");
-        System.out.println("\t 0 - da dobijes opcije.");
-        System.out.println("\t 1 - da dobijes spisak namirnica");
-        System.out.println("\t 2 - da dodas namirnicu na spisak.");
-        System.out.println("\t 3 - da izmenis namirnicu.");
-        System.out.println("\t 4 - da izbacis namirnicu sa spiska.");
-        System.out.println("\t 5 - da pronadjes namirnicu u spisku.");
-        System.out.println("\t 6 - da napustis aplikaciju.");
-    }
+
 
 }
