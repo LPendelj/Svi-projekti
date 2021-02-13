166bc080da4abc2098630158b3e8d14de6be5dd9
LPendelj <pendeljluka@gmail.com>
2021-02-13 03:42:18 +0100

    Dodao sam drugu verziju plejliste

--- Plejlista/src/plejlistaPackage/Album.java
+++ Plejlista/src/plejlistaPackage/Album.java
@@ -6,43 +6,72 @@ import java.util.LinkedList;
 public class Album {
 	private String ime;
 	private String izvodjac;
-	private ArrayList<Pesma> pesme;
+	private ListaPesama pesme;
+	//private ArrayList<Pesma> pesme;
 	
 	public Album(String ime, String izvodjac) {
 		super();
 		this.ime = ime;
 		this.izvodjac = izvodjac;
-		this.pesme = new ArrayList<Pesma>();
+		//this.pesme = new ArrayList<Pesma>();
+		this.pesme = new ListaPesama();
+	}
+	
+	private class ListaPesama{
+		private ArrayList<Pesma> pesme;
+		
+		public ListaPesama() {
+			this.pesme = new ArrayList<Pesma>();
+		}
+		
+		public boolean dodajPesmu(Pesma pesma) {
+			if(pesme.contains(pesma)) {
+				return false;
+			}
+			this.pesme.add(pesma);
+			return true;
+		}
+		
+		private Pesma nadjiPesmu(String naziv) {
+			for(Pesma provera:this.pesme) {
+				if (provera.getNaziv().equals(naziv)) {
+					return provera;
+				}
+			}
+			return null;
+		}
+		
+		public Pesma nadjiPesmu(int broj) {
+			int i = broj-1;
+			if((broj>0) && (broj<pesme.size())) {
+				return pesme.get(i);
+			}
+			return null;
+		}
+		
 	}
 	
 	public boolean dodajPesmu(String naziv, double duzina) {
-		if (nadjiPesmu(naziv)==null) {
-			this.pesme.add(new Pesma(naziv, duzina));
-			return true;
-		}
-		return false;
+		return this.pesme.dodajPesmu(new Pesma(naziv, duzina));
+		
+		
+		
 	}
 	
-	private Pesma nadjiPesmu(String naziv) {
-		for(Pesma provera:this.pesme) {
-			if (provera.getNaziv().equals(naziv)) {
-				return provera;
-			}
-		}
-		return null;
-	}
+	
 	public boolean dodajPesmu(int brojPesme, LinkedList<Pesma> plejlista) {
-		int index = brojPesme - 1;
-		if ((index >= 0) && (index < this.pesme.size())){
-			plejlista.add(this.pesme.get(index));
-			return true;
-		}
+		Pesma proverena = this.pesme.nadjiPesmu(brojPesme);
+			if(proverena != null) {
+				plejlista.add(proverena);
+				return true;
+			}
+		
 		System.out.println("album nema pesmu broj " + brojPesme);
 		return false;
 	}
 	
 	public boolean dodajPesmu(String nazivPesme, LinkedList<Pesma> plejlista) {
-		Pesma provera = nadjiPesmu(nazivPesme);
+		Pesma provera = pesme.nadjiPesmu(nazivPesme);
 		if(provera != null) {
 			plejlista.add(provera);
 			return true;
